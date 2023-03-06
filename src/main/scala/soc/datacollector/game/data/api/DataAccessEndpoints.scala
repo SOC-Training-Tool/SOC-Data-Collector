7a9ccce0149c60b63ad25c6f306ab9fc810524e3
gregoryberkman <g.herman27@gmail.com>
2023-03-05 20:19:04 -0500

    add demo client

--- src/main/scala/soc/datacollector/game/data/api/DataAccessEndpoints.scala
+++ src/main/scala/soc/datacollector/game/data/api/DataAccessEndpoints.scala
@@ -1,16 +1,14 @@
 package soc.datacollector.game.data.api
 
-import io.github.gaelrenoux.tranzactio.doobie.Database
 import io.github.gaelrenoux.tranzactio.doobie
-import io.github.gaelrenoux.tranzactio.doobie.Database.Database
 import io.grpc.Status
+import io.soc.data.game_data_service._
 import io.soc.data.{game_data_core => api}
-import io.soc.data.game_data_service.{GetGameInfoByIdRequest, GetGameInfoForPlayerRequest, GetGameInfoResponse, GetGameMovesRequest, GetGameMovesResponse, ZioGameDataService}
-import soc.datacollector.{GameId, handler}
 import soc.datacollector.game.data.{DataAccessService, GameDataQueries}
 import soc.datacollector.player.PlayerId
-import zio.{Has, ZIO}
+import soc.datacollector.{GameId, handler}
 import zio.stream.ZStream
+import zio.{Has, ZIO}
 
 object DataAccessEndpoints extends ZioGameDataService.ZGameDataService[doobie.Database.Database with Has[GameDataQueries.Service], Any] {
 
@@ -37,11 +35,11 @@ object DataAccessEndpoints extends ZioGameDataService.ZGameDataService[doobie.Da
     } yield GetGameInfoResponse(responseGameId, gameInfo.platform, responsePlayerIds, Some(gameInfo.board.data))
   }
 
-  override def getGameMoves(request: GetGameMovesRequest): ZStream[doobie.Database.Database with Has[GameDataQueries.Service] with Any, Status, GetGameMovesResponse] = {
+  override def getGameRawMoves(request: GetGameRawMovesRequest): ZStream[doobie.Database.Database with Has[GameDataQueries.Service] with Any, Status, GetGameRawMovesResponse] = {
     val gameId = ZIO.getOrFailWith(Status.INVALID_ARGUMENT)(request.id).map(gId => GameId(gId.id.toInt))
     for {
       gameId <- ZStream.fromEffect(gameId)
       move <- DataAccessService.getMoves(gameId).mapError(handler.mapError)
-    } yield GetGameMovesResponse(Some(move.data))
+    } yield GetGameRawMovesResponse(Some(move.data))
   }
 }
